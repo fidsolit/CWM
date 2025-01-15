1fea093521367fb3079bc88d2f1c708698af49da
Fidelito Pardillo <63651138+fidsolit@users.noreply.github.com>
2025-01-15 16:33:59 +0800

    modified getcategory route and product

--- cwmwebapp/app/api/common/category/getCategory/route.ts
+++ cwmwebapp/app/api/common/category/getCategory/route.ts
@@ -2,28 +2,30 @@ import connectDB from "@/DB/connectDB";
 // import AuthCheck from "@/middleware/AuthCheck";
 
 import { NextResponse } from "next/server";
-import Category from "@/model/Category";
+// import Category from "@/model/Category";\
+import Category from "@/models/Category";
 
-
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export async function GET(req: Request) {
   await connectDB();
   try {
-
-    
-    
-
-    
-      const getData = await Category.find({});
-      if (getData) {
-        return NextResponse.json({success  :true , data : getData});
-      } else {
-        return NextResponse.json({status: 204 , success: false, message: 'No categories found.' });
-      }
-   
+    const getData = await Category.find({});
+    if (getData) {
+      return NextResponse.json({ success: true, data: getData });
+    } else {
+      return NextResponse.json({
+        status: 204,
+        success: false,
+        message: "No categories found.",
+      });
+    }
   } catch (error) {
-    console.log('Error in getting all categories:', error);
-    return NextResponse.json({status : 500 , success: false, message: 'Something went wrong. Please try again!' });
+    console.log("Error in getting all categories:", error);
+    return NextResponse.json({
+      status: 500,
+      success: false,
+      message: "Something went wrong. Please try again!",
+    });
   }
 }
