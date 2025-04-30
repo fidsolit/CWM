4da7ea4d3ed0dd7f4c4367e39b833ec64a91890e
Fidelito Pardillo <63651138+fidsolit@users.noreply.github.com>
2025-05-01 01:17:18 +0800

    removing unused codes in auth ts and modified authcheck

--- cwmwebapp/middleware/AuthCheck.ts
+++ cwmwebapp/middleware/AuthCheck.ts
@@ -19,26 +19,3 @@ const AuthCheck = async (req: Request) => {
 };
 
 export default AuthCheck;
-
-// import jwt, { JwtPayload } from "jsonwebtoken";
-
-// export const dynamic = "force-dynamic";
-
-// const AuthCheck = async (req: Request) => {
-//   const token = req.headers.get("Authorization")?.split(" ")[1];
-//   if (!token) {
-//     return false;
-//   }
-//   try {
-//     const decoded = (await jwt.verify(
-//       token,
-//       // process.env.JWT_SECREAT ?? "default_secret_dumbScret"
-//       process.env.JWT_SECREAT ?? "deafault_secretkey"
-//     )) as JwtPayload;
-//     if (decoded) return decoded?.role;
-//   } catch (error) {
-//     return false;
-//   }
-// };
-
-// export default AuthCheck;
